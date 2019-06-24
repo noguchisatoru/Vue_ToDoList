8ca008ab1a46064f2e371371d9eb22365b9d2c2c
野口慧 <satoru@noguchitoshinoMacBook-Pro.local>
2019-06-24 21:29:26 +0900

    タスクの追加・削除

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ToDoリスト</h1>
+    <div>
+      <form>
+        <label><input type="radio" value="all" v-model="radioStatus" checked="true">すべて</label>
+        <label><input type="radio" value="working" v-model="radioStatus">作業中</label>
+        <label><input type="radio" value="complete" v-model="radioStatus">完了</label>
+      </form>
+    </div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>コメント</th>
+            <th>状態</th>
+            <th></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="(todo) in todos" :key="todo">
+            <th>{{ todos.indexOf(todo) }}</th>
+            <th>{{ todo.text }}</th>
+            <th><button v-bind:class="todo.state" v-on:click="change(todos.indexOf(todo))">{{ changeStatus(todo.state) }}</button></th>
+            <th><button id="remove" v-on:click="removeTodo(todos.indexOf(todo))">削除</button></th>
+          </tr>  
+        </tbody>
+      </table>
+    </div>
+    <div>
+      <h2>新規タスクの追加</h2>
+      <input type="text" v-model.trim="addtext" placeholder="すること">
+      <button v-on:click="addTodo">追加</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function(){
+    return{
+      radioStatus: "",
+      index: 0,
+      addtext: "",
+      todos: []
+    }
+  },
+
+  methods: {
+  //作業中・完了ボタンが押されたとき
+    change: function(el) {
+      if(this.todos[el].state === "work"){
+        this.$set(this.todos[el],"state","end");
+      }else{
+        this.$set(this.todos[el],"state","work");
+      }
+    },
+
+    //ボタンの表記
+    changeStatus: function(el) {
+      if(el === "work"){
+        return "作業中";
+      }else{
+        return "完了";
+      }
+    },
+
+    //タスクの追加
+    addTodo: function(){
+      this.todos.push({id:this.index, text:this.addtext, state:"work"});
+      this.addtext = '';
+      this.index++;
+    },
+
+    //タスクの削除
+    removeTodo: function(el) {
+      this.todos.splice(el, 1);
+      this.index--;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
